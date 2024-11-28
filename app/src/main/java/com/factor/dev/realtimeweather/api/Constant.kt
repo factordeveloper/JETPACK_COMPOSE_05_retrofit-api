897b53f8b283e090e6c1a43a968bd58f1b6f018b
Factor Developer <felipeherrerapinzon@gmail.com>
2024-11-28 00:17:54 -0500

    Retrofit API request

---
+++ app/src/main/java/com/factor/dev/realtimeweather/api/Constant.kt
@@ -0,0 +1,6 @@
+package com.factor.dev.realtimeweather.api
+
+object Constant {
+
+    val apiKey = "YOUR_API_KEY"
+}
---
+++ app/src/main/java/com/factor/dev/realtimeweather/api/WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.factor.dev.realtimeweather.api
+
+data class WeatherModel(
+    val current: Current,
+    val location: Location
+)
